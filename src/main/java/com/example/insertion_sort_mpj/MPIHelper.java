4314a1a6b15bfc06ccade8e9ce4948e6fad55765
genndy007 <kochevgenazp@gmail.com>
2022-06-29 20:29:13 +0300

    Insertion sort works on naked arrays and every process successfully gets his part of array

--- src/main/java/com/example/insertion_sort_mpj/MPIHelper.java
+++ src/main/java/com/example/insertion_sort_mpj/MPIHelper.java
@@ -2,26 +2,27 @@ package com.example.insertion_sort_mpj;
 
 import mpi.MPI;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MPIHelper {
     public static final int MASTER_PROCESS_ID = 0;
-    public static void generateArrayBcast(int arraySize) {
-        int process_id = MPI.COMM_WORLD.Rank();
-        if (process_id == MASTER_PROCESS_ID) {
-            List<Integer> initialArray = ListHelper.generateRandomList(arraySize);
+//    public static void generateArrayBcast(int arraySize) {
+//        int process_id = MPI.COMM_WORLD.Rank();
+//        if (process_id == MASTER_PROCESS_ID) {
+//            List<Integer> initialArray = ListHelper.generateRandomList(arraySize);
+//        }
+//    }
+
+
+    public static int[] takeSubArrayMPI(int[] array) {
+        int processId = MPI.COMM_WORLD.Rank();
+        int processesNum = MPI.COMM_WORLD.Size();
+        int chunkSize = array.length / processesNum;
+
+        if (processId == processesNum - 1) {
+            return Arrays.copyOfRange(array, processId * chunkSize, array.length);
         }
-    }
-
-
-    public static List<Integer> takeSubListMPI(List<Integer> array) {
-        int process_id = MPI.COMM_WORLD.Rank();
-        int processes_num = MPI.COMM_WORLD.Size();
-        int chunkSize = array.size() / processes_num;
-
-        if (process_id == processes_num - 1) {
-            return array.subList(process_id * chunkSize, array.size());
-        }
-        return array.subList(process_id * chunkSize, (process_id + 1) * chunkSize);
+        return Arrays.copyOfRange(array, processId * chunkSize, (processId + 1) * chunkSize);
     }
 }
--- src/main/java/com/example/insertion_sort_mpj/Main.java
+++ src/main/java/com/example/insertion_sort_mpj/Main.java
@@ -4,24 +4,26 @@ package com.example.insertion_sort_mpj;
 import mpi.*;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int arraySize = 10;
+        int arraySize = 20;
         MPI.Init(args);
         int processId = MPI.COMM_WORLD.Rank();
         int processesNum = MPI.COMM_WORLD.Size();
 
         int[] initialArray = new int[arraySize];
         if (processId == MPIHelper.MASTER_PROCESS_ID) {
-            for (int i = 0; i < arraySize; i++) {
-                initialArray[i] = i + 2;
-            }
+            initialArray = ArrayHelper.generateRandomArray(arraySize);
+            System.out.println(Arrays.toString(initialArray));
+            InsertionSort.sortSequential(initialArray);
+            System.out.println(Arrays.toString(initialArray));
         }
         MPI.COMM_WORLD.Bcast(initialArray, 0, arraySize, MPI.INT, MPIHelper.MASTER_PROCESS_ID);
 
-        System.out.println("Process" + processId + ":" + Arrays.toString(initialArray));
+        int[] subArray = MPIHelper.takeSubArrayMPI(initialArray);
+
+        System.out.println("Process" + processId + ":" + Arrays.toString(subArray));
 
 
         MPI.Finalize();
