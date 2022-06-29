bb5bb8ad0d9e63b3a1729b327bfd9d9a68fe405c
genndy007 <kochevgenazp@gmail.com>
2022-06-29 20:40:17 +0300

    Restructure code to be concise and main function only call final function

--- src/main/java/com/example/insertion_sort_mpj/MPIHelper.java
+++ src/main/java/com/example/insertion_sort_mpj/MPIHelper.java
@@ -3,17 +3,9 @@ package com.example.insertion_sort_mpj;
 import mpi.MPI;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class MPIHelper {
     public static final int MASTER_PROCESS_ID = 0;
-//    public static void generateArrayBcast(int arraySize) {
-//        int process_id = MPI.COMM_WORLD.Rank();
-//        if (process_id == MASTER_PROCESS_ID) {
-//            List<Integer> initialArray = ListHelper.generateRandomList(arraySize);
-//        }
-//    }
-
 
     public static int[] takeSubArrayMPI(int[] array) {
         int processId = MPI.COMM_WORLD.Rank();
@@ -25,4 +17,24 @@ public class MPIHelper {
         }
         return Arrays.copyOfRange(array, processId * chunkSize, (processId + 1) * chunkSize);
     }
+
+    public static void runMPISort(String[] MPI_args, int arraySize) {
+        MPI.Init(MPI_args);
+        int processId = MPI.COMM_WORLD.Rank();
+        int processesNum = MPI.COMM_WORLD.Size();
+
+        int[] initialArray = new int[arraySize];
+        if (processId == MPIHelper.MASTER_PROCESS_ID) {
+            initialArray = ArrayHelper.generateRandomArray(arraySize);
+            System.out.println(Arrays.toString(initialArray));
+        }
+        MPI.COMM_WORLD.Bcast(initialArray, 0, arraySize, MPI.INT, MPIHelper.MASTER_PROCESS_ID);
+
+        int[] subArray = MPIHelper.takeSubArrayMPI(initialArray);
+        System.out.println("Process" + processId + ":" + Arrays.toString(subArray));
+
+
+        MPI.Finalize();
+    }
+
 }
--- src/main/java/com/example/insertion_sort_mpj/Main.java
+++ src/main/java/com/example/insertion_sort_mpj/Main.java
@@ -1,33 +1,10 @@
 package com.example.insertion_sort_mpj;
 
 
-import mpi.*;
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int arraySize = 20;
-        MPI.Init(args);
-        int processId = MPI.COMM_WORLD.Rank();
-        int processesNum = MPI.COMM_WORLD.Size();
-
-        int[] initialArray = new int[arraySize];
-        if (processId == MPIHelper.MASTER_PROCESS_ID) {
-            initialArray = ArrayHelper.generateRandomArray(arraySize);
-            System.out.println(Arrays.toString(initialArray));
-            InsertionSort.sortSequential(initialArray);
-            System.out.println(Arrays.toString(initialArray));
-        }
-        MPI.COMM_WORLD.Bcast(initialArray, 0, arraySize, MPI.INT, MPIHelper.MASTER_PROCESS_ID);
-
-        int[] subArray = MPIHelper.takeSubArrayMPI(initialArray);
-
-        System.out.println("Process" + processId + ":" + Arrays.toString(subArray));
-
-
-        MPI.Finalize();
-
+        MPIHelper.runMPISort(args, arraySize);
 
     }
 }
